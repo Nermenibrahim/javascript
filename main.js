057aa9123f5e902f9f405773f092785ad6632748
Nermenibrahim <you@example.com>
2023-10-03 12:32:56 +0300

    first

---
+++ main.js
@@ -0,0 +1,124 @@
+function average(n1,n2,n3,n4,n5,n6,n7,n8,n9,n10){
+    
+    var sum= n1+n2+n3+n4+n5+n6+n6+n8+n9+n10;
+    return sum/10
+    
+
+}
+
+console.log(average(1,2,3,4,5,6,7,8,9,10))
+
+
+function rectangle(w,h){
+    return w*h/2
+
+}
+
+console.log(rectangle(20,40))
+
+
+
+function age(years){
+
+    return years*360
+
+}
+
+console.log(age(23))
+
+
+function calc(num1,num2){
+    return num1+num2
+
+}
+
+console.log(calc(12,5))
+
+
+
+function names(){
+    var name=["ahmed","adham","nada","nour","nermen","sara","menna","maiar","mohamed","yahia","adam"]
+    return name[0]
+}
+console.log(names())
+
+
+function myName(name){
+    return name
+}
+document.write("nermeen" + "" + "ibrahim" + "" +"lashin")
+
+
+function hours(hour){
+    return hour*60*60
+}
+
+console.log(hours(3))
+
+
+
+function summ(n1,n2,n3,n4){
+    
+if (n1+n2+n3+n4 > 350) {
+    return n1+n2+n3+n4 + " " + "true"
+    
+  } else {
+    return false;
+  }
+}
+document.write(summ(100,100,70,100))
+
+
+function check(num){
+
+    if (num == 0){
+        return true
+    } else{
+    return false
+    }
+}
+console.log(check(10))
+
+
+
+function moduls(n1,n2){
+    return n1%n2
+
+}
+console.log(moduls(22,10))
+
+
+function des(f,l){
+    return Math.max(f,l) 
+    if (f=l){
+        return equal
+    }
+
+}
+
+console.log(des(12,11))
+
+
+ function number(num){
+
+      return Number.isInteger(num)
+ }
+
+
+ console.log(number("ahmed"))
+
+
+function hist(){
+
+
+    
+ let date = new Date()
+
+ return date; }
+
+console.log(hist())
+
+
+
+
+
